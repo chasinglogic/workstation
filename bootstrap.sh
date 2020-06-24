bea6e785d44067c63fd67209916f15975d698326
Mathew Robinson <mathew@chasinglogic.io>
2020-06-24 13:15:45 +0100

    Write a bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing prereqs..."
+sudo apt install -y ansible build-essential python3
+
+echo "Installing roles"
+ansible-galaxy install -r requirements.yml
+
+echo "Running playbook"
+ansible-playbook playbook.yml

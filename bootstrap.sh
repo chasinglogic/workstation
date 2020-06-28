1aade2fe22d144039674e8580fca745bdaf65488
Mathew Robinson <mathew@chasinglogic.io>
2020-06-28 23:48:02 +0100

    Revert "Update to Fedora"
    This reverts commit ac94818d623ab1ad4753198846bd2ddffe547806.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing prereqs..."
-sudo dnf install -y ansible make python
+sudo apt install -y ansible build-essential python3
 
 echo "Installing roles"
 ansible-galaxy install -r requirements.yml
 
 echo "Running playbook"
-ansible-playbook -i inventory playbook.yml
+ansible-playbook playbook.yml

d108e9ad7ae33ed1424500d47ea465b6dd423ec7
Mathew Robinson <mathew@chasinglogic.io>
2023-03-21 09:06:38 +0000

    fix: add support for fedora and ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Installing prereqs..."
-sudo apt install -y ansible build-essential python3
+if [[ -f $(which apt) ]]; then
+    sudo apt install -y ansible build-essential python3
+else
+    sudo dnf install -y ansible
+fi
 
 echo "Installing roles"
 ansible-galaxy install -r requirements.yml

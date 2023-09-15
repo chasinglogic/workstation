75d3d0b9927f9d7368ab4c95f21adce41dacfd0a
Mathew Robinson <mathew@chasinglogic.io>
2023-09-15 08:33:37 +0100

    WIP: start adding macos support

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@
 echo "Installing prereqs..."
 if [[ -f $(which apt) ]]; then
     sudo apt install -y ansible build-essential python3
+elif [[ -f $(which brew) ]]; then
+    brew install ansible
 else
     sudo dnf install -y ansible
 fi
